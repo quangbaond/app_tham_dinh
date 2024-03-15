a6c9b87eb53c3c52c1337ca68ee42298164e515d
quangbaond <quangbaorp@gmail.com>
2024-03-15 21:01:45 +0700

    develop

--- screen/RegisterScreen.js
+++ screen/RegisterScreen.js
@@ -1,34 +1,81 @@
-// RegisterScreen.js
+// LoginScreen.js
 import React, { useState } from 'react';
-import { View, TextInput, Button } from 'react-native';
+import { View, TextInput, StyleSheet, ImageBackground, Text } from 'react-native';
+import { Button } from 'react-native-elements';
 
 const RegisterScreen = ({ navigation }) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-  const handleLogin = () => {
-    // Logic kiểm tra đăng nhập ở đây
-    if (username === 'admin' && password === 'admin') {
-      navigation.navigate('Main');
-    }
-  };
+    const handleLogin = () => {
+        // Logic kiểm tra đăng nhập ở đây
+        if (username === 'admin' && password === 'admin') {
+            navigation.navigate('Main');
+        }
+    };
 
-  return (
-    <View>
-      <TextInput
-        placeholder="Username"
-        value={username}
-        onChangeText={setUsername}
-      />
-      <TextInput
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
-      <Button title="Login" onPress={handleLogin} />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <ImageBackground source={require('../assets/logo/logo.jpg')} resizeMode="cover" style={styles.image}>
+                <Text style={{ color: '#fff', fontSize: 25, marginBottom: 20 }}>{process.env.EXPO_PUBLIC_APP_NAME}</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Số điện thoại"
+                    value={username}
+                    onChangeText={setUsername}
+                    placeholderTextColor="#fff"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Mật khẩu"
+                    value={password}
+                    onChangeText={setPassword}
+                    placeholderTextColor="#fff"
+                    secureTextEntry
+                />
+                    <Button title="Đăng nhập" onPress={handleLogin} style={{ width: '100%' }} />
+                    <Text
+                        onPress={() => navigation.navigate('Login')}
+                        style={{ color: '#fff', marginTop: 20, textAlign: 'center' }}>
+                        Bạn đã có tài khoản? Đăng nhập ngay
+                    </Text>
+            </ImageBackground>
+        </View>
+    );
 };
+
+const styles = StyleSheet.create({
+
+    image: {
+        flex: 1,
+        justifyContent: "center",
+        width: '100%',
+        height: '100%',
+        alignContent: 'center',
+        alignItems: 'center',
+    },
+    container: {
+        flex: 1,
+        fontFamily: 'Roboto',
+    },
+    actions: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        margin: 10,
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 5,
+        width: '90%',
+        marginBottom: 20,
+        padding: 10,
+        color: '#fff',
+        backgroundColor: 'rgba(255,255,255,0.2)',
+    }
+
+
+});
+
 
 export default RegisterScreen;
